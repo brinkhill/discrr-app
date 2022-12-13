78fa835427465401b1c9b6d858eea4d9bec1b33b
Brinkley Hill <8brink@gmail.com>
2022-12-13 15:24:43 -0500

    search component changed to have two words

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -14,9 +14,13 @@ export class ResultsComponent {
   response: any;
   skeleton: boolean = true;
   fdLink: string = '';
+
   public search() {
     this.http
-      .get('https://discit-api.fly.dev/disc?name=' + this.discName)
+      .get(
+        'https://discit-api.fly.dev/disc?name=' +
+          this.discName.replace(/\s+/g, '-').toLowerCase()
+      )
       .subscribe((response) => {
         this.response = [];
         this.response = response;
@@ -24,7 +28,7 @@ export class ResultsComponent {
         this.skeleton = false;
         this.fdLink =
           'https://foundationdiscs.com/collections/discraft/products/' +
-          this.response[0].name;
+          this.response[0].name_slug;
       });
   }
 }
