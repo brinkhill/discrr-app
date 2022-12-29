65877b5e7f8c9b79d495cb65287ce3cba5a5624c
Brinkley Hill <8brink@gmail.com>
2022-12-28 22:17:48 -0500

    changes

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Input } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { ApiService } from '../api.service';
 
@@ -14,6 +15,24 @@ export class ResultsComponent {
   response: any;
   skeleton: boolean = true;
   fdLink: string = '';
+  errorTruth: boolean;
+  rand: any;
+
+  ngOnInit() {
+    this.http.get('https://discit-api.fly.dev/disc').subscribe((response) => {
+      this.response = [];
+      this.response = response;
+      this.rand = getRandomInt(931);
+      this.response = this.response[this.rand];
+      this.skeleton = true;
+      this.fdLink =
+        'https://foundationdiscs.com/collections/discraft/products/' +
+        this.response[0].name_slug;
+    });
+    function getRandomInt(max: any) {
+      return Math.floor(Math.random() * max);
+    }
+  }
 
   public search() {
     this.http
@@ -21,17 +40,38 @@ export class ResultsComponent {
         'https://discit-api.fly.dev/disc?name=' +
           this.discName.replace(/\s+/g, '-').toLowerCase()
       )
-      .subscribe((response) => {
-        this.response = [];
-        this.response = response;
-        console.log(this.response);
-        this.skeleton = false;
-        this.fdLink =
-          'https://foundationdiscs.com/collections/discraft/products/' +
-          this.response[0].name_slug;
-      });
+      .subscribe(
+        (response) => {
+          this.response = [];
+          this.response = response;
+          console.log(this.response);
+          this.skeleton = false;
+          this.fdLink =
+            'https://foundationdiscs.com/collections/discraft/products/' +
+            this.response[0].name_slug;
+          if (response) {
+            this.errorTruth = false;
+          }
+        },
+        (error) => {
+          //Error callback
+          this.errorTruth = true;
+          console.log(this.errorTruth);
+          throw error; //You can also throw the error to a global error handler
+        }
+      );
   }
   public hideKeyboard() {
     (document.activeElement as HTMLElement).blur();
+  }
+  public copyContent() {
+    let copytext =
+      'Brand: ' + this.response.brand + ' Name: ' + this.response.name;
+    try {
+      navigator.clipboard.writeText(copytext);
+      console.log('Content copied to clipboard');
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
   }
 }
