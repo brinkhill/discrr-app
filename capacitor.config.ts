e51d0083958a55a7e7525c33242411a5f3f50a4f
Brinkley Hill <8brink@gmail.com>
2023-01-06 16:52:01 -0500

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.discrr',
+  appName: 'discrr-app',
+  webDir: 'dist/discrr-app',
+  bundledWebRuntime: false
+};
+
+export default config;
